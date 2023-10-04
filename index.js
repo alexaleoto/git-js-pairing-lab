0ff59bc542aecece5ccaae27a3385ef629546ae6
CrisM05 <Max42113@gmail.com>
2023-10-04 14:24:26 -0400

    Finished function 6

--- index.js
+++ index.js
@@ -35,3 +35,13 @@ const multiply = (num1, num2) => {
     return num1*num2
 }
 //console.log(multiply(6,9))
+
+const add = (num1, num2) => {
+    if(num1 === num2) {
+        return (num1+num2) *3;
+    } else {
+        return (num1+num2);
+    }
+}
+// console.log(add(6,9));
+// console.log(add(9,9));
